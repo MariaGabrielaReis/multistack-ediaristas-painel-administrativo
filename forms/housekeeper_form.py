69d7d53a8f91f68dcfc7ea6304a7e8f9a3a7242d
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-06-16 15:59:30 -0300

    Tradução das colunas do banco de dados para geração do form em português

--- forms/housekeeper_form.py
+++ forms/housekeeper_form.py
@@ -4,8 +4,8 @@ from ..models import Housekeeper
 
 class HousekeeperForm(forms.ModelForm):
     cpf = forms.CharField(widget=forms.TextInput(attrs={'data-mask': '000.000.000-00'}))
-    address_cep = forms.CharField(widget=forms.TextInput(attrs={'data-mask': '00000-000'}))
-    phone_number = forms.CharField(widget=forms.TextInput(attrs={'data-mask': '(00) 00000-0000'}))
+    cep = forms.CharField(widget=forms.TextInput(attrs={'data-mask': '00000-000'}))
+    telefone = forms.CharField(widget=forms.TextInput(attrs={'data-mask': '(00) 00000-0000'}))
 
     class Meta:
         model = Housekeeper
@@ -15,10 +15,10 @@ class HousekeeperForm(forms.ModelForm):
         cpf = self.cleaned_data['cpf']
         return cpf.replace('.', '').replace('-', '')
 
-    def clean_address_cep(self):
-        address_cep = self.cleaned_data['address_cep']
-        return address_cep.replace('-', '')
+    def clean_cep(self):
+        cep = self.cleaned_data['cep']
+        return cep.replace('-', '')
 
-    def clean_phone_number(self):
-        phone_number = self.cleaned_data['phone_number']
-        return phone_number.replace('(', '').replace(')', '').replace(' ', '').replace('-', '')
+    def clean_telefone(self):
+        telefone = self.cleaned_data['telefone']
+        return telefone.replace('(', '').replace(')', '').replace(' ', '').replace('-', '')
