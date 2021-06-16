6e252082da2b17bf76ef6bf6ea0c9fb0023a2f45
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-06-16 15:32:18 -0300

    Ajuste na função de tratamento dos dados do CEP
    Alteração do nome da variável de 'cep' para 'address_cep'

--- forms/housekeeper_form.py
+++ forms/housekeeper_form.py
@@ -4,7 +4,7 @@ from ..models import Housekeeper
 
 class HousekeeperForm(forms.ModelForm):
     cpf = forms.CharField(widget=forms.TextInput(attrs={'data-mask': '000.000.000-00'}))
-    cep = forms.CharField(widget=forms.TextInput(attrs={'data-mask': '00000-000'}))
+    address_cep = forms.CharField(widget=forms.TextInput(attrs={'data-mask': '00000-000'}))
     phone_number = forms.CharField(widget=forms.TextInput(attrs={'data-mask': '(00) 00000-0000'}))
 
     class Meta:
@@ -15,9 +15,9 @@ class HousekeeperForm(forms.ModelForm):
         cpf = self.cleaned_data['cpf']
         return cpf.replace('.', '').replace('-', '')
 
-    def clean_cep(self):
-        cep = self.cleaned_data['cep']
-        return cep.replace('-', '')
+    def clean_address_cep(self):
+        address_cep = self.cleaned_data['address_cep']
+        return address_cep.replace('-', '')
 
     def clean_phone_number(self):
         phone_number = self.cleaned_data['phone_number']
