f5abce058851d018d77bfd42b3044bddbe402962
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-06-16 15:24:21 -0300

    Uso de plugins e pacotes para aplicação de máscaras nos campos
    Para aplicação de máscaras nos campos do formulário, foi utilizado o plugin JQuery Mask, já para usar na listagem de diaristas, foi usado um pacote do Django chamado 'easy-mask', fazendo o tratamento dos dados antes de serem cadastrados (tira a máscara para salvar os dados)

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Housekeeper(models.Model):
     full_name = models.CharField(max_length=100, null=False, blank=False)
-    cpf = models.CharField(max_length=12, null=False, blank=False, unique=True)
+    cpf = models.CharField(max_length=11, null=False, blank=False, unique=True)
     email = models.EmailField(null=False, blank=False, unique=True)
     phone_number = models.CharField(max_length=11, null=False, blank=False)
     address = models.CharField(max_length=60, null=False, blank=False)
